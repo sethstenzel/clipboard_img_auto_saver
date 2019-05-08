c5e9749e74d3284bfb943ddbe213d35d3054e45f
seth-c-stenzel <seth.c.stenzel@gmail.com>
2019-05-08 12:17:32 -0500

    Changed batch file

--- start_stop.bat
+++ start_stop.bat
@@ -1,8 +1,10 @@
 @echo off
 echo Setting working directory: %~dp0
 cd %~dp0
-REM user python instead of pythonw to see verbose output.
-REM start python clipboard_img_auto_saver.py
+
+REM use pythonw instead of python to hide python window.
+REM start pythonw clipboard_img_auto_saver.py
+
 start python clipboard_img_auto_saver.py
 echo Press any key to continue and kill script...
 pause
